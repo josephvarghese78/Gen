c6093bd5089f2bcd8ecec66b2a4b2e1590b2b55f
josephvarghese78 <37336072+josephvarghese78@users.noreply.github.com>
2026-03-16 21:52:22 -0400

    Update s2.py

--- s2.py
+++ s2.py
@@ -1,16 +1,38 @@
-        element_viewport =self.driver.execute_script("""
-                var rect = arguments[0].getBoundingClientRect();
-                return (
-                    rect.top >= 0 &&
-                    rect.left >= 0 &&
-                    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-                    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-                );
-            """, element)
-        element_intreactable=self.driver.execute_script("""
-                var rect = arguments[0].getBoundingClientRect();
-                var x = rect.left + rect.width / 2;
-                var y = rect.top + rect.height / 2;
-                var elementAtPoint = document.elementFromPoint(x, y);
-                return arguments[0].contains(elementAtPoint) || arguments[0] === elementAtPoint;
-            """, element)
+def che(e):
+    element_viewport = c.driver.execute_script("""
+                    var rect = arguments[0].getBoundingClientRect();
+                    return (
+                        rect.top >= 0 &&
+                        rect.left >= 0 &&
+                        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+                    );
+                """, e)
+
+
+
+
+
+    element_intreactable = c.driver.execute_script("""
+            var rect = arguments[0].getBoundingClientRect();
+            var x = rect.left + rect.width / 2;
+            var y = rect.top + rect.height / 2;
+            var elementAtPoint = document.elementFromPoint(x, y);
+            return arguments[0].contains(elementAtPoint) || arguments[0] === elementAtPoint;
+        """, e)
+
+
+
+
+
+
+    element_b = c.driver.execute_script("""
+            var rect = arguments[0].getBoundingClientRect();
+            var x = rect.left + rect.width / 2;
+            var y = rect.top + rect.height / 2;
+            var elementAtPoint = document.elementFromPoint(x, y);
+            if (arguments[0].contains(elementAtPoint) || arguments[0] === elementAtPoint)
+                return null
+            else
+                return elementAtPoint;
+        """, e)
